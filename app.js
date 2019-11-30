ec969200adb1c7a6fb4aa4bfd21fe589c6157351
Arbias <52889109+k-arbias@users.noreply.github.com>
2019-11-30 15:37:56 +0100

    Disabled submit on enter

--- app.js
+++ app.js
@@ -155,9 +155,17 @@ const App = (function(ItemCtrl, UICtrl){
         const UISelectors = UICtrl.getSelectors();
 
         document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit)
-
+        //Disable submit on enter
+        document.addEventListener('keypress', function(e){
+            if(e.keyCode == 13 || e.which === 13){
+                e.preventDefault();
+                return false;
+            }
+        });
         //Edit icon click event
-        document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
+        document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
+        //Update item event
+        document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
     }
 
     const itemAddSubmit = function(e){
@@ -177,8 +185,8 @@ const App = (function(ItemCtrl, UICtrl){
         }
         e.preventDefault();
     }
-    //Update item submit
-    const itemUpdateSubmit = function(e){
+    //Click edit item 
+    const itemEditClick = function(e){
         if(e.target.classList.contains('edit-item')){
             //Get list item id
             const listId = e.target.parentNode.parentNode.id;
@@ -193,6 +201,11 @@ const App = (function(ItemCtrl, UICtrl){
             //Add item to form
             UICtrl.addItemToForm();
         }
+
+        e.preventDefault();
+    }
+    //Update item submit
+    const itemUpdateSubmit = function(e){
 
         e.preventDefault();
     }
